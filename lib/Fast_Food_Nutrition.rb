5642d353ed5cb9c1630f19db0814bf3dd38e881a
Daniel Hernandez <elreymcd@yahoo.com>
2018-06-16 10:34:34 -0500

    Creating Gem Setup

---
+++ lib/Fast_Food_Nutrition.rb
@@ -0,0 +1,2 @@
+require_relative "./Fast_Food_Nutrition/version"
+require_relative "./Fast_Food_Nutrition/cli"
---
+++ lib/Fast_Food_Nutrition/cli.rb
@@ -0,0 +1,8 @@
+class Welcome
+
+  def intro
+    puts "Welcome To The Fast Food Nutrition Finder"
+    #list_restaurants
+  end
+
+end
